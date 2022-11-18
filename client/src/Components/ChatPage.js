64c12516719f7466016ecd62de4364cb62f28504
Ruaridhi <ruaridhiscanlain@yahoo.co.uk>
2022-11-18 14:30:42 +0000

    create basic comps

---
+++ client/src/Components/ChatPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Message from './Message';
+
+export default function ChatPage() {
+  return (
+    <div>
+      <h1>ChatPage</h1>
+      <Message />
+    </div>
+  );
+}
---
+++ client/src/Components/Message.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Massage() {
+  return <div>Massage</div>;
+}
